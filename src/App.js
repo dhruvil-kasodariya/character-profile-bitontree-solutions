d9e0a83ed94d2cb05fc8ea9445f0611c755452cc
dhruvil-kasodariya <kasodariyadhruvil45@gmail.com>
2024-01-04 21:15:03 +0530

    set React project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className='text-[gray]'>Hello <br/><span className='text-[rgba(59,130,246,.5);]'>Bitontree</span> Solution</h1>
+    </div>
+  );
+}
+
+export default App;
